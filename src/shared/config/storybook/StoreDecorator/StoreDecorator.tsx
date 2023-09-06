40ddf9883061e4ec985d2781734250c8d3078323
YanaPronko <YaniaR@tut.by>
2023-09-06 18:26:09 +0300

    feat: add async reducers to Store Provider

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,17 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Decorator } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUserName';
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>): Decorator => (Story) => (
-  <StoreProvider initialState={state}>
+const defaultAsyncReducers = {
+  loginForm: loginReducer,
+};
+
+export const StoreDecorator = (
+  state: DeepPartial<StateSchema>,
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
+): Decorator => (Story) => (
+  <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
     <Story />
   </StoreProvider>
 );
