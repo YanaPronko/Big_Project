189075a3c08fb29b09049a5d0000b053dc4fc239
Yana Pronko <yaniar@tut.by>
2023-09-12 15:54:56 +0300

    refactor: add some refactors

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,2 +1,1 @@
-export { Profile, ProfileSchema } from './model/types/profile';
-export { profileSlice, profileActions, profileReducer } from './model/slice/profileSlice';
+export { ProfileCard } from './ui/ProfileCard/ProfileCard';
