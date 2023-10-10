5763ba538972f6c6ea2b53ffe2f90fa6bfb0a67a
Yana Pronko <yaniar@tut.by>
2023-10-11 00:44:05 +0300

    fix: fix interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,9 +3,6 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-  },
 });
 
 $api.interceptors.request.use((config) => {
