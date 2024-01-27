02bbeeb34461ad8a498ba29608d48ac61fe9e19f
Yana Pronko <yaniar@tut.by>
2024-01-28 02:49:37 +0300

    feat: add ssl certificate to server

--- jsonServer/index.js
+++ jsonServer/index.js
@@ -1,10 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const jsonServer = require('json-server');
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+
+const jsonServer = require("json-server");
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+};
 
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
@@ -18,23 +25,23 @@ server.use(async (req, res, next) => {
 });
 
 // Эндпоинт для логина
-server.post('/login', (req, res) => {
+server.post("/login", (req, res) => {
   try {
     const { username, password } = req.body;
     const db = JSON.parse(
-      fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8')
+      fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8"),
     );
     const { users = [] } = db;
 
     const userFromBd = users.find(
-      (user) => user.username === username && user.password === password
+      (user) => user.username === username && user.password === password,
     );
 
     if (userFromBd) {
       return res.json(userFromBd);
     }
 
-    return res.status(403).json({ message: 'User not found' });
+    return res.status(403).json({ message: "User not found" });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: e.message });
@@ -45,7 +52,7 @@ server.post('/login', (req, res) => {
 // eslint-disable-next-line
 server.use((req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(403).json({ message: 'AUTH ERROR' });
+    return res.status(403).json({ message: "AUTH ERROR" });
   }
 
   next();
@@ -54,6 +61,8 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-  console.log('server is running on 8000 port');
+const PORT = 8443;
+const httpsServer = https.createServer(options, server);
+httpsServer.listen(PORT, () => {
+  console.log(`server is running on ${PORT} port`);
 });
