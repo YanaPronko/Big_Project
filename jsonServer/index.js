4e4981de1247e4fe9ab4fa060e9a420136b21839
Yana Pronko <yaniar@tut.by>
2024-01-30 10:36:43 +0300

    feat: add ssl keys and change server

--- jsonServer/index.js
+++ jsonServer/index.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
-const https = require("https");
+const http = require("http");
+// const https = require("https");
 const path = require("path");
 
 const jsonServer = require("json-server");
 
-const options = {
-  key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-  cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
-};
+// const options = {
+//   key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+//   cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+// };
 
 const server = jsonServer.create();
 
@@ -61,8 +62,16 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-const PORT = 8443;
-const httpsServer = https.createServer(options, server);
-httpsServer.listen(PORT, () => {
-  console.log(`server is running on ${PORT} port`);
+// const PORT = 8443;
+const HTTP_PORT = 8000;
+
+// const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+// httpsServer.listen(PORT, () => {
+//   console.log(`server is running on ${PORT} port`);
+// });
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`server is running on ${HTTP_PORT} port`);
 });
