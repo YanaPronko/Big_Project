3585721248380fed2761c76ce1186d53935d5b0c
Yana Pronko <yaniar@tut.by>
2024-03-08 12:09:25 +0300

    feat: add new branch newDesign, fix articleListItemSkeleton

--- src/entities/Article/ui/ArticleListItem/ArtcileListItemSkeleton/ArticleListItemSkeleton.tsx
+++ src/entities/Article/ui/ArticleListItem/ArtcileListItemSkeleton/ArticleListItemSkeleton.tsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
 
 import { classNames } from "@/shared/lib/classNames/classNames";
-import { toggleFeatures } from "@/shared/lib/featureFlags";
-import { Card as CardDeprecated } from "@/shared/ui/deprecated/Card";
+import { ToggleFeatures, toggleFeatures } from "@/shared/lib/featureFlags";
+import { Card } from "@/shared/ui/deprecated/Card";
 import { Skeleton as SkeletonDeprecated } from "@/shared/ui/deprecated/Skeleton";
 import { CardRedesigned } from "@/shared/ui/redesigned/Card";
 import { SkeletonRedesigned } from "@/shared/ui/redesigned/Skeleton";
@@ -27,43 +27,76 @@ export const ArticleListItemSkeleton = memo(
       off: () => SkeletonDeprecated,
     });
 
-    const Card = toggleFeatures({
-      name: "isAppRedesigned",
-      on: () => CardRedesigned,
-      off: () => CardDeprecated,
-    });
-
     if (view === "list") {
       return (
-        <div className={classNames(mainClass, {}, [className, cls[view]])}>
-          <Card className={cls.card} max padding="24">
-            <div className={cls.header}>
-              <Skeleton borderRadius="50%" height={30} width={30} />
-              <Skeleton width={150} height={16} className={cls.username} />
-              <Skeleton width={150} height={16} className={cls.date} />
+        <ToggleFeatures
+          feature="isAppRedesigned"
+          on={
+            <div className={classNames(mainClass, {}, [className, cls[view]])}>
+              <CardRedesigned className={cls.card} max padding="24">
+                <div className={cls.header}>
+                  <Skeleton borderRadius="50%" height={30} width={30} />
+                  <Skeleton width={150} height={16} className={cls.username} />
+                  <Skeleton width={150} height={16} className={cls.date} />
+                </div>
+                <Skeleton width={250} height={24} className={cls.title} />
+                <Skeleton height={200} className={cls.img} />
+                <div className={cls.footer}>
+                  <Skeleton height={36} width={200} className={cls.btn} />
+                </div>
+              </CardRedesigned>
             </div>
-            <Skeleton width={250} height={24} className={cls.title} />
-            <Skeleton height={200} className={cls.img} />
-            <div className={cls.footer}>
-              <Skeleton height={36} width={200} className={cls.btn} />
+          }
+          off={
+            <div className={classNames(mainClass, {}, [className, cls[view]])}>
+              <Card className={cls.card} max>
+                <div className={cls.header}>
+                  <Skeleton borderRadius="50%" height={30} width={30} />
+                  <Skeleton width={150} height={16} className={cls.username} />
+                  <Skeleton width={150} height={16} className={cls.date} />
+                </div>
+                <Skeleton width={250} height={24} className={cls.title} />
+                <Skeleton height={200} className={cls.img} />
+                <div className={cls.footer}>
+                  <Skeleton height={36} width={200} className={cls.btn} />
+                </div>
+              </Card>
             </div>
-          </Card>
-        </div>
+          }
+        />
       );
     }
 
     return (
-      <div className={classNames(mainClass, {}, [className, cls[view]])}>
-        <Card className={cls.card} borderRadius="round" padding="0">
-          <div className={cls.imageWrapper}>
-            <Skeleton width="100%" className={cls.img} />
+      <ToggleFeatures
+        feature="isAppRedesigned"
+        on={
+          <div className={classNames(mainClass, {}, [className, cls[view]])}>
+            <CardRedesigned className={cls.card} borderRadius="round" padding="0">
+              <div className={cls.imageWrapper}>
+                <Skeleton width="100%" className={cls.img} />
+              </div>
+              <div className={cls.infoWrapper}>
+                <Skeleton width="100%" />
+              </div>
+              <Skeleton width="100%" height={16} className={cls.title} />
+            </CardRedesigned>
           </div>
-          <div className={cls.infoWrapper}>
-            <Skeleton width="100%" />
+        }
+        off={
+          <div className={classNames(mainClass, {}, [className, cls[view]])}>
+            <Card className={cls.card}>
+              <div className={cls.imageWrapper}>
+                <Skeleton width="100%" className={cls.img} />
+              </div>
+              <div className={cls.infoWrapper}>
+                <Skeleton width="100%" />
+              </div>
+              <Skeleton width="100%" height={16} className={cls.title} />
+            </Card>
           </div>
-          <Skeleton width="100%" height={16} className={cls.title} />
-        </Card>
-      </div>
+        }
+      />
     );
   },
 );
