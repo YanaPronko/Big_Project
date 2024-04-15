4a31b1e4a70f16babebc404fe12e2ff4c0e7c6b7
YanaPronko <YaniaR@tut.by>
2024-04-15 16:34:38 +0300

    fix: fix server

--- jsonServer/index.js
+++ jsonServer/index.js
@@ -1,14 +1,40 @@
 const fs = require("fs");
-// const https = require("https");
+const https = require("https");
 
 const jsonServer = require("json-server");
 const path = require("path");
 const http = require("http");
 
-// const options = {
-//   key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-//   cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
-// };
+
+function getOptions() {
+  if (
+    fs.existsSync(
+      path.resolve(
+        __dirname,
+        "/etc/letsencrypt/live/openblogapp.ru/privkey.pem",
+      ),
+    )
+  ) {
+    return {
+      key: fs.readFileSync(
+        path.resolve(
+          __dirname,
+          "/etc/letsencrypt/live/yaniarweb.com/privkey.pem",
+        ),
+      ),
+      cert: fs.readFileSync(
+        path.resolve(
+          __dirname,
+          "/etc/letsencrypt/live/yaniarweb.com/fullchain.pem",
+        ),
+      ),
+    };
+  }
+  return {};
+}
+
+const options = getOptions();
+
 
 const server = jsonServer.create();
 
@@ -63,15 +89,15 @@ server.use(router);
 
 // запуск сервера
 
-// const PORT = 8443;
+const PORT = 8443;
 const HTTP_PORT = 8000;
 
-// const httpsServer = https.createServer(options, server);
+const httpsServer = https.createServer(options, server);
 const httpServer = http.createServer(server);
 
-// httpsServer.listen(PORT, () => {
-//   console.log(`server is running on ${PORT} port`);
-// });
+httpsServer.listen(PORT, () => {
+  console.log(`server is running on ${PORT} port`);
+});
 
 httpServer.listen(HTTP_PORT, () => {
   console.log(`server is running on ${HTTP_PORT} port`);
